2f74eeec90e5a92319b0ad7ef996ca8e67e90419
Jeremy Bejarano <jmbejara@gmail.com>
2024-04-05 22:50:04 -0400

    Add SphinxBook docs and introduce doit

---
+++ cmds/config.py
@@ -0,0 +1,42 @@
+"""Load project configurations from .env files.
+Provides easy access to paths and credentials used in the project.
+Meant to be used as an imported module.
+
+If `config.py` is run on its own, it will create the appropriate
+directories.
+
+For information about the rationale behind decouple and this module,
+see https://pypi.org/project/python-decouple/
+
+Note that decouple mentions that it will help to ensure that
+the project has "only one configuration module to rule all your instances."
+This is achieved by putting all the configuration into the `.env` file.
+You can have different sets of variables for difference instances, 
+such as `.env.development` or `.env.production`. You would only
+need to copy over the settings from one into `.env` to switch
+over to the other configuration, for example.
+
+"""
+from decouple import config
+from pathlib import Path
+import pandas as pd
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATA_DIR = (BASE_DIR / config('DATA_DIR', default=Path('data'), cast=Path)).resolve()
+OUTPUT_DIR = (BASE_DIR / config('OUTPUT_DIR', default=Path('output'), cast=Path)).resolve()
+WRDS_USERNAME = config("WRDS_USERNAME", default="")
+QUOTE_DATE = config("QUOTE_DATE", default="2023-06-30", cast=pd.to_datetime)
+
+
+if __name__ == "__main__":
+    
+    # ## If they don't exist, create the data and output directories
+    # (DATA_DIR / 'pulled').mkdir(parents=True, exist_ok=True)
+
+    # Sometimes, I'll create other folders to organize the data
+    # (DATA_DIR / 'intermediate').mkdir(parents=True, exist_ok=True)
+    # (DATA_DIR / 'derived').mkdir(parents=True, exist_ok=True)
+
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    Path("./docs/_notebook_build").mkdir(parents=True, exist_ok=True)
---
+++ docs/index.rst
@@ -0,0 +1,46 @@
+.. MY Proj documentation master file, created by
+   sphinx-quickstart on Wed Feb 14 16:47:54 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to My Blank Project's documentation!
+============================================
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   ./_notebook_build/_D.0. Introduction.ipynb
+   ./_notebook_build/_D.1. Treasury Debt.ipynb
+   ./_notebook_build/_D.1.X. Debt Markets.ipynb
+   ./_notebook_build/_D.2. Money Markets.ipynb
+   ./_notebook_build/_D.3. Inflation.ipynb
+   ./_notebook_build/_D.4. Commodity Futures.ipynb
+   ./_notebook_build/_D.6. Currency.ipynb
+   myst_markdown_demos.md
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
